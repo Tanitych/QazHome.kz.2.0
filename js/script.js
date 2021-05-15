7451d9122d60033f6fa0b650e0f97f6cf70f9cfb
Tanitych <Tanitych@gmail.com>
2021-05-16 01:11:26 +0600

    almost done

--- js/script.js
+++ js/script.js
@@ -105,13 +105,10 @@ document.querySelector('.tabs__nav__btn:nth-child(1)').click();
 var slider = document.getElementById("myRange");
 var output = document.getElementById("value");
 
-
 output.innerHTML = slider.value;
-
 slider.oninput = function () {
     output.innerHTML = this.value;
-}
-
+};
 slider.addEventListener("input", function () {
     var maxAttribute = this.getAttribute('max');
     var x = slider.value;
@@ -125,11 +122,9 @@ var slider2 = document.getElementById("myRange2");
 var output2 = document.getElementById("value2");
 
 output2.innerHTML = slider2.value;
-
 slider2.oninput = function () {
     output2.innerHTML = this.value;
-}
-
+};
 slider2.addEventListener("input", function () {
     var maxAttribute = this.getAttribute('max');
     var x = slider2.value;
@@ -138,6 +133,134 @@ slider2.addEventListener("input", function () {
     slider2.style.background = color;
 });
 
+
+var slider3 = document.getElementById("myRange3");
+var output3 = document.getElementById("value3");
+
+output3.innerHTML = slider3.value;
+slider3.oninput = function () {
+    output3.innerHTML = this.value;
+};
+slider3.addEventListener("input", function () {
+    var maxAttribute = this.getAttribute('max');
+    var x = slider3.value;
+    var valueWidth = ((x * 100) / maxAttribute);
+    var color = 'linear-gradient(90deg, rgb(5, 178, 205)' + valueWidth + '%, rgb(199, 199, 204)' + valueWidth + '%)';
+    slider3.style.background = color;
+});
+
+
+var slider4 = document.getElementById("myRange4");
+var output4 = document.getElementById("value4");
+
+output4.innerHTML = slider4.value;
+slider4.oninput = function () {
+    output4.innerHTML = this.value;
+};
+slider4.addEventListener("input", function () {
+    var maxAttribute = this.getAttribute('max');
+    var x = slider4.value;
+    var valueWidth = ((x * 100) / maxAttribute);
+    var color = 'linear-gradient(90deg, rgb(5, 178, 205)' + valueWidth + '%, rgb(199, 199, 204)' + valueWidth + '%)';
+    slider4.style.background = color;
+});
+
+var slider5 = document.getElementById("myRange5");
+var output5 = document.getElementById("value5");
+
+output5.innerHTML = slider5.value;
+slider5.oninput = function () {
+    output5.innerHTML = this.value;
+};
+slider5.addEventListener("input", function () {
+    var maxAttribute = this.getAttribute('max');
+    var x = slider5.value;
+    var valueWidth = ((x * 100) / maxAttribute);
+    var color = 'linear-gradient(90deg, rgb(5, 178, 205)' + valueWidth + '%, rgb(199, 199, 204)' + valueWidth + '%)';
+    slider5.style.background = color;
+});
+
+
+var slider6 = document.getElementById("myRange6");
+var output6 = document.getElementById("value6");
+
+output6.innerHTML = slider6.value;
+slider6.oninput = function () {
+    output6.innerHTML = this.value;
+};
+slider6.addEventListener("input", function () {
+    var maxAttribute = this.getAttribute('max');
+    var x = slider6.value;
+    var valueWidth = ((x * 100) / maxAttribute);
+    var color = 'linear-gradient(90deg, rgb(5, 178, 205)' + valueWidth + '%, rgb(199, 199, 204)' + valueWidth + '%)';
+    slider6.style.background = color;
+});
+
+
+
+var slider7 = document.getElementById("myRange7");
+var output7 = document.getElementById("value7");
+
+output7.innerHTML = slider7.value;
+slider7.oninput = function () {
+    output7.innerHTML = this.value;
+};
+slider7.addEventListener("input", function () {
+    var maxAttribute = this.getAttribute('max');
+    var x = slider7.value;
+    var valueWidth = ((x * 100) / maxAttribute);
+    var color = 'linear-gradient(90deg, rgb(5, 178, 205)' + valueWidth + '%, rgb(199, 199, 204)' + valueWidth + '%)';
+    slider7.style.background = color;
+});
+
+
+
+var slider8 = document.getElementById("myRange8");
+var output8 = document.getElementById("value8");
+
+output8.innerHTML = slider8.value;
+slider8.oninput = function () {
+    output8.innerHTML = this.value;
+};
+slider8.addEventListener("input", function () {
+    var maxAttribute = this.getAttribute('max');
+    var x = slider8.value;
+    var valueWidth = ((x * 100) / maxAttribute);
+    var color = 'linear-gradient(90deg, rgb(5, 178, 205)' + valueWidth + '%, rgb(199, 199, 204)' + valueWidth + '%)';
+    slider8.style.background = color;
+});
+
+
+var slider9 = document.getElementById("myRange9");
+var output9 = document.getElementById("value9");
+
+output9.innerHTML = slider9.value;
+slider9.oninput = function () {
+    output9.innerHTML = this.value;
+};
+slider9.addEventListener("input", function () {
+    var maxAttribute = this.getAttribute('max');
+    var x = slider9.value;
+    var valueWidth = ((x * 100) / maxAttribute);
+    var color = 'linear-gradient(90deg, rgb(5, 178, 205)' + valueWidth + '%, rgb(199, 199, 204)' + valueWidth + '%)';
+    slider9.style.background = color;
+});
+
+
+var slider10 = document.getElementById("myRange10");
+var output10 = document.getElementById("value10");
+
+output10.innerHTML = slider10.value;
+slider10.oninput = function () {
+    output10.innerHTML = this.value;
+};
+slider10.addEventListener("input", function () {
+    var maxAttribute = this.getAttribute('max');
+    var x = slider10.value;
+    var valueWidth = ((x * 100) / maxAttribute);
+    var color = 'linear-gradient(90deg, rgb(5, 178, 205)' + valueWidth + '%, rgb(199, 199, 204)' + valueWidth + '%)';
+    slider10.style.background = color;
+});
 
 //mask
 $(function () {
